c061783055b9680df62164e4da693e16fd4b42d1
mv <mariaverobledo@gmail.com>
2026-08-07 16:13:37 -0300

    Rompo la prueba a proposito para probar el quality gate

--- src/pages/Login.ts
+++ src/pages/Login.ts
@@ -10,7 +10,7 @@ export class LoginPage {
     constructor (page: Page){
         this.page = page;
 
-        this.inputUsername = page.locator('#usernameeeeeee');
+        this.inputUsername = page.locator('#usernameeeeeeeeeee');
         this.inputPass = page.locator('#password');
         this.btnLogin = page.locator('#submit');
     }
